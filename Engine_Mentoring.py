c1fdb52427cbd94d598924279bfa2683edd580ca
Paul Gamble <paulgamble@Hippocraticai-Paul-Gamble.local>
2024-01-23 12:58:42 -0800

    fresh

--- Engine_Mentoring.py
+++ Engine_Mentoring.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import json
 import ast
-from firebase.firebase_utils import write_task_item
+#from firebase.firebase_utils import write_task_item
 
 # wide
 st.set_page_config(layout="wide")
 
-FB_DB = "1-23-multi-engine-mentoring-2"
+#FB_DB = "1-23-multi-engine-mentoring-2"
 
 data_fn = "mmem_1_17_B.csv"
 DATASET = pd.read_csv(data_fn)
@@ -75,55 +75,65 @@ def sample_data_row(DATASET):
 data_obj = sample_data_row(DATASET)
 
 st.header("Myelin Engine Mentoring")
-user_name = st.text_input("Enter your name:")
+#user_name = st.text_input("Enter your name:")
+st.subheader("Prompt ID: " + data_obj['PROMPT_ID'])
 
 with st.expander("**Checklist**"):
     #st.subheader("Checklist")
     st.write(data_obj['PREAMBLE'])
 
-with st.form(key='feedback', clear_on_submit=True):
+#with st.form(key='feedback', clear_on_submit=True):
 
-    col1, col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
-    with col1:
-        with st.container(border=True):
-            st.subheader("Conversation")
-            st.write(data_obj['CONV'])
+with col1:
+    with st.container(border=True):
+        st.subheader("Conversation")
+        st.write(data_obj['CONV'])
 
-    task_feedback = {}
-    with col2:
-        with st.container(border=True):
-            st.subheader("Tasks")
-            #st.write(TASKS)
-            for task in data_obj['RAW_TASKS']:
-                with st.container(border=True):
-                    st.write("**" + task['pointer'] + " " + task['engine'] + "**")
-                    st.write(task['content'])
-                    task_feedback[task['pointer']] = st.radio("This task is:", ("Active", "Inactive", "An Error"), key=task['pointer'])
+#task_feedback = {}
+with col2:
+    with st.container(border=True):
+        st.subheader("Tasks")
+        #st.write(TASKS)
+        for task in data_obj['RAW_TASKS']:
+            with st.container(border=True):
+                st.write("**" + task['pointer'] + " " + task['engine'] + "**")
+                st.write(task['content'])
+                #task_feedback[task['pointer']] = st.radio("This task is:", ("Active", "Inactive", "An Error"), key=task['pointer'])
 
 
-    st.subheader("Feedback")
-    #st.write("Please provide feedback on the conversation and tasks. What do you like? What do you not like? What would you change?")
-
-    response_feedback = {}
+with st.container(border=True):
+    st.subheader("Responses")
+    # response_feedback = {}
     for j, response in enumerate(data_obj['RESPONSES']):
-        st.write(response)
-        response_feedback[response] = st.slider("How would you rate this response?", 0, 7, key="response_" + str(j))
+        st.write("Response " + str(j) + ": " + response)
+#     response_feedback[response] = st.slider("How would you rate this response?", 0, 7, key="response_" + str(j))
+
+with st.container(border=True):
+    st.subheader("Feedback")
+    st.write("Please provide your feedback in the google form. Be sure to copy the item_id from above!")
 
 
-    rewrite = st.text_area(label="Write the ideal response:", height=200)
+# #st.write("Please provide feedback on the conversation and tasks. What do you like? What do you not like? What would you change?")
 
-    submitted = st.form_submit_button(label='Submit')
 
-    if submitted:
-        #st.write("Thank you for your feedback!")
-        write_task_item(
-            {
-                "user_name": user_name,
-                "data_obj": data_obj,
-                "response_feedback": str(response_feedback),
-                "rewrite": rewrite,
-                "task_feedback": str(task_feedback)
-            },
-            FB_DB
-        )
+
+
+# rewrite = st.text_area(label="Write the ideal response:", height=200)
+
+# submitted = st.form_submit_button(label='Submit')
+
+# if submitted:
+#     #st.write("Thank you for your feedback!")
+#     # write_task_item(
+#     #     {
+#     #         "user_name": user_name,
+#     #         "data_obj": data_obj,
+#     #         "response_feedback": str(response_feedback),
+#     #         "rewrite": rewrite,
+#     #         "task_feedback": str(task_feedback)
+#     #     },
+#     #     FB_DB
+#     # )
+#     pass
